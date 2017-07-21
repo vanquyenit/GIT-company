03791b458e412d629a15d4d5e1282c78fd548574
tannguyenit <tannguyenit95@gmail.com>
2017-07-21 21:09:01 +0700

    init project

---
+++ package.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<style type="text/css">
+	p {
+		color: #d45818;
+	}
+</style>
+<body>
+<h2>1, Emmet.</h2>
+<p>
+	Đây là package cho phép chúng ta gõ tắt code HTML-CSS cách dùng là dùng các cú pháp gõ tắt mà emmet hỗ trợ sau đó ấn tab để thực hiên.
+</p>
+<!-- <img src="https://i.ytimg.com/vi/EmML7H8p1iI/maxresdefault.jpg" width="1000px" height="350px"> -->
+
+
+
+
+<h2>2, Bracket HighLighter.</h2>
+
+<p>
+	-Đây là package giúp cho các bạn có thể dễ nhìn ra phần mở/ đóng của thẻ nằm ở chỗ nào.
+</p>
+<!-- <img src="http://images0.cnblogs.com/blog2015/720466/201507/181805004071706.png" width="1000px" height="350px"> -->
+
+
+<h2>3, Color HighLighter.</h2>
+
+<p>
+	-Package này giúp bạn code css hay hơn bằng cách hiển thị luôn màu sắc của mã màu.
+</p>
+
+
+
+<h2>4, SidebarEnhancements.</h2>
+
+<p>
+	-Package này tạo ra các context menu với nhiều tiện ích tốt như open in browser......
+</p>
+
+
+
+<h2>5, Git.</h2>
+
+<p>
+	-Package này giúp bạn có thể làm việc với GIT một cách đơn giản bằng các câu lệnh sẵn có...
+</p>
+
+
+
+<h2>6, AdvancedNewFile.</h2>
+
+<p>
+	-Package này cho phép bạn lưu file một cách chuyên nghiệp hơn. Để sử dụng nhấn tổ hợp phím Ctrl + Alt +N.
+</p>
+
+
+
+<h2>
+	8, AutoPrefixer.
+</h2>
+
+<p>
+	-Package này rất cần thiết cho các bạn làm front-end, Nó giúp tạo ra các prefix cho các trình duyệt tương ứng.
+</p>
+
+
+<h2>
+	9, HTML-CSS-JS Prettify.
+</h2>
+
+<p>
+	-Đôi khi code của chúng ta sẽ rất lộn xộn và khó có thể đọc được, và package này giúp cho code của chúng ta được fomat lại theo đúng chuẩn. Hỗ trợ HTML, CSS, Javascript và JSON, để fomat gõ tổ hợp phím Ctrl + shift+ h.
+</p>
+
+
+
+<h2>
+	10, DocBlockr.
+</h2>
+
+<p>
+	-Package này cho phép các bạn tạo ra các comment đúng chuẩn.
+-package này giúp các bạn có thể dễ dàng tìm ra các function,class,.. đang sử dụng được viết từ đâu...
+</p>
+
+
+
+<h2>
+	12, AutoFileName.
+</h2>
+
+<p>
+	-Package này sẽ hiển thị ra tất cả các file có trong thư mục để các bạn có thể nhúng file đơn giản hơn.
+</p>
+
+
+
+<h2>
+	
+13, GIT Gutter
+</h2>
+
+<p>
+	Plug-in để hiển thị thông tin về các tập tin trong một kho git.
+</p>
+</body>
+</html>
